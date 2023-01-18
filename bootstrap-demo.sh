baea5ccdbbace3e1f6ffb1744187837f87fbcf3f
Aaron E <aaron.evans@hashicorp.com>
2023-01-18 21:26:18 +1100

    init

---
+++ bootstrap-demo.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+# Unset out current status of the Environment Variables
+unset VAULT_PORT
+unset VAULT_TOKEN
+unset VAULT_ADDR
+unset ROLE_NAME
+unset AWS_ACCESS_KEY_ID
+unset AWS_SECRET_ACCESS_KEY
+unset AWS_SESSION_TOKEN
+unset TFC_AGENT_NAME
+
+# Set up Vault specific Env variables
+# VAULT_LICENSE env variable requires a valid Vault license key
+export VAULT_PORT=8200
+export VAULT_TOKEN=root
+export VAULT_ADDR=http://localhost:${VAULT_PORT}
+export ROLE_NAME=testrole
+export TFC_AGENT_NAME=tfc-agent
+
+# Run Terraform Cloud Agent
+if [ "$(docker ps -q -f name=$TFC_AGENT_NAME)" ]; then
+  echo "tfc-agent container is running"
+else
+  echo "---starting tfc-agent container---"
+  docker pull cloudbrokeraz/tfc-agent-custom:latest
+  docker run -d --rm --name tfc-agent --network host --cap-add=IPC_LOCK \
+-e "TFC_AGENT_TOKEN=${TFC_AGENT_TOKEN}" \
+-e "TFC_AGENT_NAME=${TFC_AGENT_NAME}" \
+cloudbrokeraz/tfc-agent-custom:latest
+fi
+
+# Set up the AWS CLI Env variables so that Vault can use them for setting up the AWS Secrets Engine
+doormat login -f && eval $(doormat aws export --account aws_aaron.evans_test)
+
+echo VAULT_PORT:    $VAULT_PORT
+echo VAULT_TOKEN:   $VAULT_TOKEN
+echo VAULT_ADDR:    $VAULT_ADDR
+echo VAULT_LICENSE: $VAULT_LICENSE
+echo ROLE_NAME:     $ROLE_NAME
+echo AWS_ACCESS_KEY_ID: $AWS_ACCESS_KEY_ID
+echo AWS_SECRET_ACCESS_KEY: $AWS_SECRET_ACCESS_KEY
+echo AWS_SESSION_TOKEN: $AWS_SESSION_TOKEN
+
+# Deploy Vault in a container rather than on localhost
+# passes in the AWS Env variables so that when the TFC agent
+# executes the vault-configuration repo (https://github.com/CloudbrokerAz/vault-configuration)
+# it has the variables to configure the AWS Secrets Engine correctly
+if [ "$(docker ps -q -f name=vault-enterprise)" ]; then
+  echo "Container is running"
+  docker kill vault-enterprise
+  sleep 5
+fi
+
+echo "---starting vault container---"
+docker run -d --rm --name vault-enterprise --cap-add=IPC_LOCK \
+-e "VAULT_DEV_ROOT_TOKEN_ID=${VAULT_TOKEN}" \
+-e "VAULT_DEV_LISTEN_ADDRESS=0.0.0.0:${VAULT_PORT}" \
+-e "AWS_SESSION_TOKEN=$AWS_SESSION_TOKEN" \
+-e "AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID" \
+-e "AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY" \
+-e "VAULT_LICENSE"=${VAULT_LICENSE} \
+-p ${VAULT_PORT}:${VAULT_PORT} \
+hashicorp/vault-enterprise:latest
+
+# Run Terraform to configure Vault back to a good state
+terraform apply -replace vault_aws_secret_backend_role.vault_role_assumed_role_credential_type -auto-approve
