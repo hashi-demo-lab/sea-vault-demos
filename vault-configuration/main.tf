8a715262b29d0be1d75e88070219bc7fd33eeb65
Aaron E <aaron.evans@hashicorp.com>
2023-01-19 23:27:05 +1100

    General updates

--- vault-configuration/main.tf
+++ vault-configuration/main.tf
@@ -3,7 +3,7 @@ provider "vault" {
 }
 
 # Create a KV secrets engine
-resource "vault_mount" "futureApp" {
+resource "vault_mount" "main" {
   path        = var.kv_secrets_mount
   type        = "kv"
   options     = { version = "2" }
@@ -11,20 +11,33 @@ resource "vault_mount" "futureApp" {
 }
 
 # Create a secret in the KV engine
-resource "vault_kv_secret_v2" "futureApp" {
-  mount = vault_mount.futureApp.path
+resource "vault_kv_secret_v2" "main" {
+  mount = vault_mount.main.path
   name  = var.kv_secrets_key
   data_json = jsonencode(
     {
-      customerName = "aaron",
-      location  = "sydney"
+      team     = "solution engineers and architects",
+      location = "sydney"
     }
   )
 }
 
-# Create a policy granting the TFC workspace access to the KV engine
-resource "vault_policy" "futureApp" {
-  name = "tfc-workspace-oidc"
+# Enable AWS Secrets Engine
+resource "vault_aws_secret_backend" "main" {
+  description = "Demo of the AWS secrets engine"
+}
+
+# Configure AWS Secrets Engine with Assumed Role
+resource "vault_aws_secret_backend_role" "main" {
+  backend         = vault_aws_secret_backend.main.path
+  credential_type = "assumed_role"
+  name            = "vault-demo-assumed-role"
+  role_arns       = ["${var.role_arns}"]
+}
+
+# Create a policy granting the TFC workspace access to the KV engine & AWS engine
+resource "vault_policy" "main" {
+  name = "demo_policy"
 
   policy = <<EOT
 # Generate child tokens with Terraform provider
@@ -38,7 +51,7 @@ capabilities = ["read"]
 }
 
 # Get secrets from KV engine
-path "${vault_kv_secret_v2.futureApp.path}" {
+path "${vault_kv_secret_v2.main.path}" {
   capabilities = ["list","read"]
 }
 
@@ -46,12 +59,11 @@ path "${vault_kv_secret_v2.futureApp.path}" {
 path "aws/*" {
   capabilities = ["create", "read", "update", "patch", "delete", "list"]
 }
-
 EOT
 }
 
 # Create the JWT auth method to use GitHub
-resource "vault_jwt_auth_backend" "jwt" {
+resource "vault_jwt_auth_backend" "main" {
   description        = "JWT Backend for TFC OIDC"
   path               = "jwt"
   oidc_discovery_url = "https://app.terraform.io"
@@ -59,10 +71,10 @@ resource "vault_jwt_auth_backend" "jwt" {
 }
 
 # Create the JWT role tied to workspaceOne
-resource "vault_jwt_auth_backend_role" "workspaceOneRole" {
-  backend           = vault_jwt_auth_backend.jwt.path
+resource "vault_jwt_auth_backend_role" "main" {
+  backend           = vault_jwt_auth_backend.main.path
   role_name         = "vault-demo-assumed-role"
-  token_policies    = [vault_policy.futureApp.name]
+  token_policies    = [vault_policy.main.name]
   token_max_ttl     = "100"
   bound_audiences   = ["vault.testing"]
   bound_claims_type = "glob"
@@ -71,16 +83,4 @@ resource "vault_jwt_auth_backend_role" "workspaceOneRole" {
   }
   user_claim = "terraform_full_workspace"
   role_type  = "jwt"
-}
-
-
-resource "vault_aws_secret_backend" "aws" {
-  description       = "Demo of the AWS secrets engine"
-}
-
-resource "vault_aws_secret_backend_role" "vault_role_assumed_role_credential_type" {
-  backend         = vault_aws_secret_backend.aws.path
-  credential_type = "assumed_role"
-  name            = "vault-demo-assumed-role"
-  role_arns       = ["arn:aws:iam::258850230659:role/testrole"]
 }
