4d6daf3ea3ac6408d2be03c39257c67cfccb222e
Aaron E <aaron.evans@hashicorp.com>
2023-01-20 00:39:15 +1100

    updated vpc reference

--- apps/aws/infra/main.tf
+++ apps/aws/infra/main.tf
@@ -66,7 +66,7 @@ resource "aws_internet_gateway" "main" {
 }
 
 resource "aws_route_table" "main" {
-  vpc_id = aws_vpc.future.id
+  vpc_id = aws_vpc.main.id
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.main.id
