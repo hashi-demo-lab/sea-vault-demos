fd7aa7cf299a7df2b1b5a7b81c11f7a276177341
Aaron E <aaron.evans@hashicorp.com>
2023-01-23 09:24:15 +1100

    mysql and clean up

--- vault-configuration/main.tf
+++ vault-configuration/main.tf
@@ -6,88 +6,7 @@ provider "aws" {
   region = var.region
 }
 
-# Create a KV secrets engine
-resource "vault_mount" "main" {
-  path        = var.kv_secrets_mount
-  type        = "kv"
-  options     = { version = "2" }
-  description = "KV mount for TFC OIDC demo"
-}
-
-# Create a secret in the KV engine
-resource "vault_kv_secret_v2" "main" {
-  mount = vault_mount.main.path
-  name  = var.kv_secrets_key
-  data_json = jsonencode(
-    {
-      team     = "solution engineers and architects",
-      location = "sydney"
-    }
-  )
-}
-
-# Enable AWS Secrets Engine
-resource "vault_aws_secret_backend" "main" {
-  description = "Demo of the AWS secrets engine"
-  path = var.VAULT_PATH
-}
-
-resource "aws_iam_role" "role" {
-  name = "test-role"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": ["sts:AssumeRole",
-                "sts:SetSourceIdentity"],
-      "Principal": {
-        "AWS": "${var.doormat_user_arn}"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_policy" "policy" {
-  name        = "test-policy"
-  description = "A test policy"
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "ec2:*"
-      ],
-      "Effect": "Allow",
-      "Resource": "*"
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_role_policy_attachment" "test-attach" {
-  role       = aws_iam_role.role.name
-  policy_arn = aws_iam_policy.policy.arn
-}
-
-
-# Configure AWS Secrets Engine with Assumed Role
-resource "vault_aws_secret_backend_role" "main" {
-  backend         = vault_aws_secret_backend.main.path
-  credential_type = "assumed_role"
-  name            = "vault-demo-assumed-role"
-  role_arns       = [aws_iam_role.role.arn]
-}
-
-# Create the JWT auth method to use GitHub
+# Enable the JWT authentication method for TFC Workload Identity
 resource "vault_jwt_auth_backend" "main" {
   description        = "JWT Backend for TFC OIDC"
   path               = "jwt"
@@ -95,7 +14,6 @@ resource "vault_jwt_auth_backend" "main" {
   bound_issuer       = "https://app.terraform.io"
 }
 
-# Create the JWT role tied to workspaceOne
 resource "vault_jwt_auth_backend_role" "main" {
   backend           = vault_jwt_auth_backend.main.path
   role_name         = "vault-demo-assumed-role"
@@ -108,4 +26,10 @@ resource "vault_jwt_auth_backend_role" "main" {
   }
   user_claim = "terraform_full_workspace"
   role_type  = "jwt"
+}
+
+# Enable the GitHub authentication method
+resource "vault_github_auth_backend" "github_authentication" {
+  organization = "hashicorp-demo-lab"
+  token_policies = [ "demo_policy" ]
 }
--- vault-configuration/variables.tf
+++ vault-configuration/variables.tf
@@ -1,18 +1,18 @@
 variable "tfc_organization" {
   type        = string
   description = "(Required) The name of the TFC organization"
-  nullable = false
+  nullable    = false
 }
 
 variable "tfc_project" {
-  type        = string 
+  type        = string
   description = "(Required) The name of the TFC Project that the workspace is located within"
-  nullable = false
+  nullable    = false
 }
 variable "tfc_workspace" {
   type        = string
   description = "(Required) The name of the TFC workspace"
-  nullable = false
+  nullable    = false
 }
 
 variable "kv_secrets_mount" {
@@ -22,35 +22,56 @@ variable "kv_secrets_mount" {
 }
 
 variable "kv_secrets_key" {
-  type        = string 
+  type        = string
   description = "Name of the key inside a K/V secret engine"
   nullable    = false
 }
 
 variable "role_arns" {
-  type        = string 
+  type        = string
   description = "Amazon Resource Name of the role in AWS IAM "
-  nullable = false
+  nullable    = false
 }
 
 variable "VAULT_PATH" {
-    type        = string
-    description = "Vault path used for mount path of secrets engine and Vault policy"
-    nullable    =  false
+  type        = string
+  description = "Vault path used for mount path of secrets engine and Vault policy"
+  nullable    = false
 }
 
 variable "vault_address" {
-  type = string
+  type        = string
   description = "vault address"
-  default = "http://localhost:8200"
+  default     = "http://localhost:8200"
 }
 
 variable "region" {
-  type = string
+  type        = string
   description = "aws region"
 }
 
 variable "doormat_user_arn" {
-  type = string
+  type        = string
   description = "arn of doormat user"
+  default     = "arn:aws:sts::258850230659:assumed-role/aws_aaron.evans_test-developer/aaron.evans@hashicorp.com"
+}
+
+variable "mysql_username" {
+  type        = string
+  description = "service account used for Vault to connect to the mysql container"
+}
+
+variable "mysql_password" {
+  type        = string
+  description = "password used for Vault to connect to the mysql container"
+}
+
+variable "mysql_application_username" {
+  type        = string
+  description = "This is static account used by an application (this could be an existing account)"
+}
+
+variable "mysql_database_name" {
+  type        = string
+  description = "Name of the database within Vault configuration"
 }
