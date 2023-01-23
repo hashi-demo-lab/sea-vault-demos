390b07a5f597fed411e72031d9b69618eb5f15cc
Aaron E <aaron.evans@hashicorp.com>
2023-01-23 15:27:37 +1100

    renamed vault-configuration

--- aws-dynamic-provider-credentials/main.tf
+++ aws-dynamic-provider-credentials/main.tf
@@ -1,7 +1,5 @@
-# Enable AWS Secrets Engine
-resource "vault_aws_secret_backend" "main" {
-  description = "Demo of the AWS secrets engine"
-  path        = var.VAULT_PATH
+provider "aws" {
+  region = var.region
 }
 
 resource "aws_iam_role" "role" {
@@ -50,10 +48,43 @@ resource "aws_iam_role_policy_attachment" "test-attach" {
   policy_arn = aws_iam_policy.policy.arn
 }
 
+
+provider "vault" {
+  address = var.vault_address
+}
+
+# Enable AWS Secrets Engine
+resource "vault_aws_secret_backend" "main" {
+  description = "Demo of the AWS secrets engine"
+  path        = var.VAULT_PATH
+}
+
 # Configure AWS Secrets Engine with Assumed Role
 resource "vault_aws_secret_backend_role" "main" {
   backend         = vault_aws_secret_backend.main.path
   credential_type = "assumed_role"
   name            = "vault-demo-assumed-role"
   role_arns       = [aws_iam_role.role.arn]
+}
+
+# Enable the JWT authentication method for TFC Workload Identity
+resource "vault_jwt_auth_backend" "main" {
+  description        = "JWT Backend for TFC OIDC"
+  path               = "jwt"
+  oidc_discovery_url = "https://app.terraform.io"
+  bound_issuer       = "https://app.terraform.io"
+}
+
+resource "vault_jwt_auth_backend_role" "main" {
+  backend           = vault_jwt_auth_backend.main.path
+  role_name         = "vault-demo-assumed-role"
+  token_policies    = [vault_policy.main.name]
+  token_max_ttl     = "100"
+  bound_audiences   = ["vault.testing"]
+  bound_claims_type = "glob"
+  bound_claims = {
+    sub = "organization:${var.tfc_organization}:project:${var.tfc_project}:workspace:${var.tfc_workspace}:run_phase:*"
+  }
+  user_claim = "terraform_full_workspace"
+  role_type  = "jwt"
 }
