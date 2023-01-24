8f8a8cbd776b74077ef20f912a5de206ac3a0f91
Aaron E <aaron.evans@hashicorp.com>
2023-01-24 23:56:37 +1100

    updates

--- mysql-dynamic-credentials/main.tf
+++ mysql-dynamic-credentials/main.tf
@@ -9,7 +9,7 @@ resource "vault_database_secrets_mount" "databases" {
     name           = var.mysql_database_name
     username       = var.mysql_username
     password       = var.mysql_password
-    connection_url = "{{username}}:{{password}}@tcp(host.docker.internal:3306)/"
+    connection_url = "{{username}}:{{password}}@tcp(172.19.0.3:3306)/"
     allowed_roles = [
       "db-user-static",
       "db-user-readonly",
@@ -23,14 +23,23 @@ resource "vault_database_secret_backend_static_role" "static_role" {
   name            = "db-user-static"
   db_name         = vault_database_secrets_mount.databases.mysql[0].name
   username        = var.mysql_application_username
-  rotation_period = 60
+  rotation_period = 120
 }
 
 resource "vault_database_secret_backend_role" "db-user-readonly" {
   backend             = vault_database_secrets_mount.databases.path
   name                = "db-user-readonly"
   db_name             = vault_database_secrets_mount.databases.mysql[0].name
-  default_ttl         = 60
-  max_ttl             = 300
+  default_ttl         = 600
+  max_ttl             = 900
   creation_statements = ["CREATE USER '{{name}}'@'%' IDENTIFIED BY '{{password}}';GRANT SELECT ON my_app.* TO '{{name}}'@'%';"]
+}
+
+resource "vault_database_secret_backend_role" "db-user-readwrite" {
+  backend             = vault_database_secrets_mount.databases.path
+  name                = "db-user-readwrite"
+  db_name             = vault_database_secrets_mount.databases.mysql[0].name
+  default_ttl         = 3600
+  max_ttl             = 7200
+  creation_statements = ["CREATE USER '{{name}}'@'%' IDENTIFIED BY '{{password}}';GRANT ALL ON *.* TO '{{name}}'@'%';"]
 }
