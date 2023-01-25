9dbc69854291a5fcf7181980bab4d04e8459bf4a
Aaron E <aaron.evans@hashicorp.com>
2023-01-25 11:13:55 +1100

    added transform and transit engine config

--- mysql-dynamic-credentials/main.tf
+++ mysql-dynamic-credentials/main.tf
@@ -4,7 +4,7 @@ provider "vault" {
 
 resource "vault_database_secrets_mount" "databases" {
   path = "demo-databases"
-
+  description = "This is an example database secret engine mount"
   mysql {
     name           = var.mysql_database_name
     username       = var.mysql_username
@@ -42,4 +42,38 @@ resource "vault_database_secret_backend_role" "db-user-readwrite" {
   default_ttl         = 3600
   max_ttl             = 7200
   creation_statements = ["CREATE USER '{{name}}'@'%' IDENTIFIED BY '{{password}}';GRANT ALL ON *.* TO '{{name}}'@'%';"]
+}
+
+resource "vault_mount" "transit" {
+  path        = "demo-transit"
+  type        = "transit"
+  description = "This is an example transit secret engine mount"
+  default_lease_ttl_seconds = 3600
+  max_lease_ttl_seconds     = 86400
+}
+
+resource "vault_transit_secret_backend_key" "key" {
+  backend = vault_mount.transit.path
+  name    = "customer-key"
+}
+
+resource "vault_mount" "transform" {
+  path = "demo-transform"
+  type = "transform"
+  description = "This is an example transform secret engine mount"
+}
+
+resource "vault_transform_role" "transform_role" {
+  path = vault_mount.transform.path
+  name = "ssn"
+  transformations = ["ssn-fpe"]
+}
+
+resource "vault_transform_transformation" "example" {
+  path          = vault_mount.transform.path
+  name          = "ssn-fpe"
+  type          = "fpe"
+  template      = "builtin/socialsecuritynumber"
+  tweak_source  = "internal"
+  allowed_roles = ["ssn"]
 }
