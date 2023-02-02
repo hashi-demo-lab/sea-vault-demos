740faa2cc56835f775ad1d77c1cad35c4d058cf9
Aaron E <aaron.evans@hashicorp.com>
2023-02-02 22:36:22 +1100

    updates to starting mysql container

--- mysql-dynamic-credentials/bootstrap-usecase.sh
+++ mysql-dynamic-credentials/bootstrap-usecase.sh
@@ -16,15 +16,14 @@ docker pull mysql/mysql-server:5.7
 docker pull cloudbrokeraz/app01:latest
 
 # Start MySQL
-echo "---STARTING MYSQL CONTAINER---"
-if [ "$(docker ps -q -f name=mysql)" ]; then
-  echo "mysql 5.7 container is running"
-  docker container rm -f mysql
+echo "\n\033[32m---STARTING MYSQL CONTAINER---\033[0m"
+if docker inspect "mysql" &> /dev/null; then
+  echo "\033[33mContainer MySQL already exists, killing container and redeploying.\033[0m"
+  docker rm -f mysql
   sleep 5
   docker volume rm app-data
   sleep 3 
 fi
-
 docker run --name mysql --hostname mysql --network demo-network \
   -v app-data:/var/lib/mysql \
   -e "MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD" \
@@ -36,26 +35,25 @@ sleep 15
 
 # Create an application service account 
 alias mysql="docker exec -it mysql mysql"
-mysql -u root -p'root' -e "CREATE USER 'dbsvc1'@'%' IDENTIFIED BY 'dbsvc1';"
-mysql -u root -p'root' -e "GRANT INSERT,SELECT,UPDATE,DELETE ON my_app.* TO 'dbsvc1'@'%';"
+mysql -u root -p'root' -e "CREATE USER 'dbsvc1'@'%' IDENTIFIED BY 'dbsvc1';" &> /dev/null
+mysql -u root -p'root' -e "GRANT INSERT,SELECT,UPDATE,DELETE ON my_app.* TO 'dbsvc1'@'%';" &> /dev/null
 
 # Create Vault servcie account for configuration of database secrets engine
-mysql -u root -proot -e "CREATE USER 'vaultadmin'@'%' IDENTIFIED BY 'vaultadmin';"
-mysql -u root -proot -e "GRANT ALL PRIVILEGES ON *.* TO 'vaultadmin'@'%' WITH GRANT OPTION;"
+mysql -u root -proot -e "CREATE USER 'vaultadmin'@'%' IDENTIFIED BY 'vaultadmin';" &> /dev/null
+mysql -u root -proot -e "GRANT ALL PRIVILEGES ON *.* TO 'vaultadmin'@'%' WITH GRANT OPTION;" &> /dev/null
 
 # Setup database secrets engine
 terraform init
 terraform apply -auto-approve
 
 # Start app
-echo "---STARTING APP CONTAINER---"
-if [ "$(docker ps -a -f name=app)" ]; then
-  echo "app container is running"
-  docker container rm -f app
+echo "\n\033[32m---STARTING TRANSIT APP CONTAINER---\033[0m"
+if docker inspect "transit-app" &> /dev/null; then
+  echo "\033[33mContainer transit-app already exists, killing container and redeploying.\033[0m"
+  docker rm -f transit-app &> /dev/null
   sleep 3
 fi
-
-docker run --network demo-network --hostname app --name app \
+docker run --network demo-network --hostname transit-app --name transit-app \
   -p 5000:5000 \
   -e VAULT_ADDR=http://vault-enterprise:8200 \
   -e VAULT_DATABASE_CREDS_PATH=demo-databases/creds/db-user-readwrite \
