e5fe6610220ac9b47df6b26a6bbf0a96414d023d
Simon Lynch <simon.lynch@hashicorp.com>
2023-02-06 10:44:03 +1100

    up

--- aws-dynamic-workload-identity-vault-customhooks/demo.auto.tfvars
+++ aws-dynamic-workload-identity-vault-customhooks/demo.auto.tfvars
@@ -4,3 +4,7 @@ tfc_project                = "Default Project"
 tfc_workspace              = "aws-dynamic-credentials"
 VAULT_PATH                 = "aws-dynamic-credentials"
 region                     = "ap-southeast-2"
+
+# workspace variables
+organization = "hashi-demos-apj"
+workspace_name = "aws-future-app"
---
+++ aws-dynamic-workload-identity-vault-customhooks/workspace.tf
@@ -0,0 +1,40 @@
+
+
+
+module "workpace" {
+  source = "github.com/hashicorp-demo-lab/terraform-tfe-onboarding-module"
+
+  organization = var.organization
+
+  create_project = var.create_project
+  project_name = var.project_name
+
+  workspace_name = var.workspace_name
+  workspace_description = var.workspace_description
+
+  workspace_terraform_version = var.workspace_terraform_version
+  workspace_tags = var.workspace_tags
+  variables = var.variables
+
+  remote_state = var.remote_state
+  remote_state_consumers = var.remote_state_consumers
+
+  vcs_repo = var.vcs_repo
+  workspace_vcs_directory = var.workspace_vcs_directory
+  workspace_auto_apply = var.workspace_auto_apply
+
+
+  workspace_agents = var.workspace_agents
+  execution_mode = var.execution_mode
+  agent_pool_name = var.agent_pool_name
+
+  assessments_enabled = var.assessments_enabled
+
+  workspace_read_access_emails = var.workspace_read_access_emails
+  workspace_write_access_emails = var.workspace_write_access_emails
+  workspace_plan_access_emails = var.workspace_plan_access_emails
+
+}
+
+
+
