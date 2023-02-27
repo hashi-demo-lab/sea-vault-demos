1eb4723efdb7c5131973305faade4d80d6401a4b
Aaron E <aaron.evans@hashicorp.com>
2023-02-27 22:28:00 +1100

    a

--- kubernetes/bootstrap-demo.sh
+++ kubernetes/bootstrap-demo.sh
@@ -77,6 +77,7 @@ for dc in "${(@k)datacentres}"; do
   done
   echo "\033[32mRoot token for ${dc}: $(eval echo "\${${dc}_root_token}")\033[0m"
   echo "\033[32mUnseal key for ${dc}: $(eval echo "\${${dc}_unseal_key}")\033[0m\n"
+  
   # Troubleshooting purposes
   # helm list
   # for i in {0..2} ; do kubectl exec vault-${dc}-$i -- vault status ; done
