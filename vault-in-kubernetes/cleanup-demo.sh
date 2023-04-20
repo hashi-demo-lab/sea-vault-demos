922aba5bc2ebc5d2e017880585d3a65f6c1d0174
Simon Lynch <simon.lynch@hashicorp.com>
2023-04-21 09:29:58 +1000

    added cleanup into script

---
+++ vault-in-kubernetes/cleanup-demo.sh
@@ -0,0 +1,13 @@
+for dc in dc1 dc2; do
+    helm uninstall vault-${dc} --namespace=my-vault-demo
+    echo "sleeping..."
+    sleep 5
+  done
+  kubectl delete pvc --all --namespace=my-vault-demo
+  kubectl delete secret my-vault-license --namespace=my-vault-demo
+  kubectl get pods -o wide --namespace=my-vault-demo
+  lsof -nP -iTCP -sTCP:LISTEN | grep 32000
+
+  kubectl get all --namespace=my-vault-demo
+  kubectl get pvc --namespace=my-vault-demo
+  kubectl get pods --namespace=my-vault-demo
