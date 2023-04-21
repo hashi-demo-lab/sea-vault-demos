e5d788838a05cf669cd661a6f795911adc4aa3ff
Aaron E <aaron.evans@hashicorp.com>
2023-04-21 14:12:09 +1000

    added variables.tf

---
+++ tfc-agent/variables.tf
@@ -0,0 +1,9 @@
+variable "agent_name" {
+  type        = string
+  description = "Terraform Cloud agent name"
+}
+
+variable "agent_token" {
+  type        = string
+  description = "Terraform Cloud agent token"
+  default = ""
