3e8f65c3b754ebb10887fb91c59858fbf790ec05
Aaron E <aaron.evans@hashicorp.com>
2023-04-25 14:06:41 +1000

    Updated script to only delete vault pvc's

--- vault-in-kubernetes/cleanup-demo.sh
+++ vault-in-kubernetes/cleanup-demo.sh
@@ -3,12 +3,19 @@ for dc in dc1 dc2; do
     echo "sleeping..."
     sleep 5
   done
-  k delete secret vault-secrets 
-  kubectl delete pvc --all --namespace=my-vault-demo
-  kubectl delete secret my-vault-license --namespace=my-vault-demo
-  kubectl get pods -o wide --namespace=my-vault-demo
-  lsof -nP -iTCP -sTCP:LISTEN | grep 32000
 
-  kubectl get all --namespace=my-vault-demo
-  kubectl get pvc --namespace=my-vault-demo
-  kubectl get pods --namespace=my-vault-demo
+for i in {0..2}; do
+  kubectl delete pvc data-vault-dc1-$i
+done
+
+for i in {0..2}; do
+  kubectl delete pvc data-vault-dc2-$i
+done
+
+kubectl delete secret my-vault-license --namespace=my-vault-demo
+kubectl get all -o wide --namespace=my-vault-demo
+kubectl get pvc --namespace=my-vault-demo
+lsof -nP -iTCP -sTCP:LISTEN | grep 32000
+
+
+
