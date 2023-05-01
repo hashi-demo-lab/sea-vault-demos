462f6ae43bd4e5d46f6e0274935b215e077db40b
Aaron E <aaron.evans@hashicorp.com>
2023-05-01 20:25:18 +1000

    updated bootstrap scripts

--- vault-disaster-recovery/bootstrap-demo.sh
+++ vault-disaster-recovery/bootstrap-demo.sh
@@ -1,19 +1,21 @@
 #!usr/bin/zsh
 
+# Configure DR on Primary Vault (dc1)
 export VAULT_ADDR='http://localhost:32000'
 export VAULT_TOKEN=$dc1_root_token
 vault login $VAULT_TOKEN
-
 vault write -f sys/replication/dr/primary/enable
 sleep 5
 secondary_token_output=$(vault write sys/replication/dr/primary/secondary-token id="dr-secondary" -format=json)
 token=$(echo $secondary_token_output | jq -r '.wrap_info.token')
 
+# Configure DR on Secondary Vault (dc2)
 export VAULT_ADDR='http://localhost:32001'
 export VAULT_TOKEN=$dc2_root_token
 vault login $VAULT_TOKEN
-
 vault write sys/replication/dr/secondary/enable token="${token}"
+
+# Unseal DR nodes with Primary Vault (dc1) unseal key
 for i in {1..2}; do
   pod_name="vault-dc2-$i"
   echo "\n\033[32mUnsealing pod: ${pod_name}\033[0m\n"
