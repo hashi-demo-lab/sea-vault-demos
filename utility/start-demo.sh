129606957cb4ae86fe11bcd875d3a6f1ac3ff4a5
Aaron E <aaron.evans@hashicorp.com>
2023-07-05 14:44:58 +1000

    updates

---
+++ utility/start-demo.sh
@@ -0,0 +1,20 @@
+#!usr/bin/zsh
+
+rm ../vault-kv-secrets-engine/*.tfstate*
+cd ../vault-in-kubernetes
+. ./cleanup-demo.sh  
+sleep 2
+. ./bootstrap-demo.sh
+sleep 2
+cd ../vault-kv-secrets-engine
+terraform apply -auto-approve
+terraform apply -auto-approve
+sleep 2
+cd ../aws-dynamic-workload-identity-vault-customhooks
+terraform apply -auto-approve
+
+clear
+for dc in dc1 dc2; do
+  echo "\033[32mRoot token for $dc: $(eval echo "\${${dc}_root_token}")\033[0m"
+  echo "\033[32mUnseal key for $dc: $(eval echo "\${${dc}_unseal_key}")\033[0m\n"
+done
