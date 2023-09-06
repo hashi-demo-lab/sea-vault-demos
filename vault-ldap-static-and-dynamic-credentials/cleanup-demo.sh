1a7718bb0d8bc1fdb9071b853b5a242aef8c4273
Aaron E <aaron.evans@hashicorp.com>
2023-09-06 21:57:48 +1000

    added terraform destroy and delete k8s secret

--- vault-ldap-static-and-dynamic-credentials/cleanup-demo.sh
+++ vault-ldap-static-and-dynamic-credentials/cleanup-demo.sh
@@ -3,5 +3,8 @@
 # Define variables
 namespace="my-vault-demo"
 
+terraform destroy --auto-approve
+
 # Delete Kubernetes resources
 kubectl delete -f ./kubernetes_config/ldap.yaml
+kubectl delete secret ldap-secrets
