71dd06239108a3ff9f0d8df8d1815b69a90e8e40
Aaron E <aaron.evans@hashicorp.com>
2023-09-06 22:01:14 +1000

    cleanup

--- vault-agent/bootstrap-demo.sh
+++ vault-agent/bootstrap-demo.sh
@@ -3,5 +3,4 @@ export VAULT_PORT=443
 export VAULT_ADDR=https://vault-dc1.hashibank.com:${VAULT_PORT}
 
 vault login $dc1_root_token
-vault secrets enable -version=2 -path=demo-key-value kv
 vault kv put /demo-key-value/hashibank @data.json
