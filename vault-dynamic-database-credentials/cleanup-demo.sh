5ea56559cfc6785c49b0d0fb4c11ee65c1aff945
Aaron E <aaron.evans@hashicorp.com>
2023-09-06 22:00:15 +1000

    cleanup script

--- vault-dynamic-database-credentials/cleanup-demo.sh
+++ vault-dynamic-database-credentials/cleanup-demo.sh
@@ -2,14 +2,10 @@
 
 # Define variables
 namespace="my-vault-demo"
-
 export VAULT_TOKEN=$dc1_root_token
-tf destroy --auto-approve
+
+terraform destroy --auto-approve
 
 # Delete Kubernetes resources
 kubectl delete -f ./kubernetes_config/transit-app.yaml
 kubectl delete -f ./kubernetes_config/mysql.yaml
-
-
-kubectl get all -o wide --namespace=my-vault-demo
-kubectl get pvc --namespace=my-vault-demo
