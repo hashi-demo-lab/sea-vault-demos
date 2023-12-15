c016dd15ee4e09b27ec72a0306117aa3e3e13086
Aaron Evans <aarone@outlook.com.au>
2023-12-15 16:00:31 +1100

    clean up

--- vault-ldap-static-and-dynamic-credentials/bootstrap-usecase.sh
+++ vault-ldap-static-and-dynamic-credentials/bootstrap-usecase.sh
@@ -1,21 +1,11 @@
 #!/bin/zsh
-# Define variables
-#namespace="my-vault-demo"
-#LDAP_ORGANISATION="engineers"
-#LDAP_DOMAIN="hashibank.com"
-#LDAP_ADMIN_PASSWORD="admin"
-
 kubectl create secret generic ldap-secrets \
     --from-file=tls.key=./cert/ldap_tls.key \
     --from-file=tls.crt=./cert/ldap_tls.crt
-
 # Apply Kubernetes YAML file
 kubectl apply -f ./kubernetes_config/ldap.yaml
-
 # Wait for ldap deployment to be ready
 sleep 10
-
 kubectl cp data.ldif my-vault-demo/ldap-0:/tmp/data.ldif -c ldap-container
 kubectl exec ldap-0 -c ldap-container -- ldapadd -cxD "cn=admin,dc=hashibank,dc=com" -w admin -f /tmp/data.ldif
-
 terraform apply -auto-approve
