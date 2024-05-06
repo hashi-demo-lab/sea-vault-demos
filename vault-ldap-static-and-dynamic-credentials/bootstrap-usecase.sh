6ba645e00517bb12e9fcde66e01f57fd975ef29d
Aaron Evans <aarone@outlook.com.au>
2024-05-06 10:17:19 +1000

    updates to ldap flow

--- vault-ldap-static-and-dynamic-credentials/bootstrap-usecase.sh
+++ vault-ldap-static-and-dynamic-credentials/bootstrap-usecase.sh
@@ -5,7 +5,7 @@ kubectl create secret generic ldap-secrets \
 # Apply Kubernetes YAML file
 kubectl apply -f ./kubernetes_config/ldap.yaml
 # Wait for ldap deployment to be ready
-sleep 10
+sleep 15
 kubectl cp data.ldif my-vault-demo/ldap-0:/tmp/data.ldif -c ldap-container
 kubectl exec ldap-0 -c ldap-container -- ldapadd -cxD "cn=admin,dc=hashibank,dc=com" -w admin -f /tmp/data.ldif
 terraform apply -auto-approve
--- vault-ldap-static-and-dynamic-credentials/cleanup-demo.sh
+++ vault-ldap-static-and-dynamic-credentials/cleanup-demo.sh
@@ -8,3 +8,6 @@ terraform destroy --auto-approve
 # Delete Kubernetes resources
 kubectl delete -f ./kubernetes_config/ldap.yaml
 kubectl delete secret ldap-secrets
+
+rm terraform.tfstate
+rm terraform.tfstate.backup
