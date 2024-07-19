c0439b06e45f4f83f812f23a12d827a89f4d146e
Aaron Evans <aarone@outlook.com.au>
2024-07-19 12:02:48 +1000

    clean up

--- ldap/main.tf
+++ ldap/main.tf
@@ -107,8 +107,6 @@ resource "vault_ldap_secret_backend_static_role" "prod_service" {
   rotation_period = 86400 # 24 hours
 }
 
-/**/
-/*
 data "external" "password_hash" {
   program = ["./files/generate_hash.sh", "your-password-here"]
 }
@@ -117,14 +115,16 @@ resource "vault_ldap_secret_backend_dynamic_role" "this" {
   for_each  = tomap({ for role in var.ldap_roles : role.role_name => role })
   mount     = vault_ldap_secret_backend.config.path
   role_name = each.value.role_name
+
+  username_template = "dyn-service-{{truncate 8 .DisplayName}}-{{random 8}}"
   creation_ldif = templatefile("${path.module}/files/creation.ldif.tmpl", {
-    group_names = each.value.group_names,
+    role_name    = each.value.role_name,
+    group_names  = each.value.group_names,
     password_hash = data.external.password_hash.result["passwordHash"]
   })
   deletion_ldif     = file("${path.module}/files/deletion.ldif")
   rollback_ldif     = file("${path.module}/files/rollback.ldif")
-  default_ttl       = 60 * 10     # One hour
-  max_ttl           = 8 * 3600 # Eight hours
-  username_template = "{{printf \"%s%s%s%s\" (.DisplayName | truncate 8) (.RoleName | truncate 8) (random 20)| truncate 20}}"
+  
+  default_ttl       = 86400     # 24 hours
+  max_ttl           = 8 * 3600  # Eight hours
 }
-*/
--- ldap/variables.tf
+++ ldap/variables.tf
@@ -6,13 +6,13 @@ variable "vault_address" {
 variable "ldap_roles" {
   description = "An object containing the role name and ldap groups to join."
   type = list(object({
-    role_name   = string
+    role_name   = string,
     group_names = list(string)
   }))
   default = [
     {
-      role_name   = "vault_ldap_dynamic_demo_role"
-      group_names = ["dev"]
+      role_name = "vault_ldap_dynamic_demo_role",
+      group_names = ["service-accounts"]
     }
   ]
 }
