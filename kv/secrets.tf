2c64bb51312807c06df8b1f0efe0f5d2030a0438
Aaron Evans <aarone@outlook.com.au>
2024-09-23 10:06:11 +1000

    style: Format JSON data for consistency in KV secrets

--- kv/secrets.tf
+++ kv/secrets.tf
@@ -21,15 +21,15 @@ resource "vault_kv_secret_v2" "team_sea_secrets" {
   name  = var.kv_team_secrets_name
   data_json = jsonencode(
     {
-      team     = "Solution Engineers and Architects",
-      location = "Australia and New Zealand"
+      team          = "Solution Engineers and Architects",
+      location      = "Australia and New Zealand"
       football_team = "Sydney Swans"
     }
   )
   custom_metadata {
     max_versions = 5
     data = {
-      demo = "true",
+      demo    = "true",
       account = "Services Australia"
     }
   }
@@ -40,8 +40,8 @@ resource "vault_kv_secret_v2" "simons_secrets" {
   name  = var.kv_simons_secrets_name
   data_json = jsonencode(
     {
-      role     = "Sr Solutions Architecture Specialist",
-      location = "Sydney"
+      role          = "Sr Solutions Architecture Specialist",
+      location      = "Sydney"
       football_team = "Cronulla Sharks"
     }
   )
@@ -52,8 +52,8 @@ resource "vault_kv_secret_v2" "aarons_secrets" {
   name  = var.kv_aarons_secrets_name
   data_json = jsonencode(
     {
-      role     = "Solutions Engineer",
-      location = "Kiama"
+      role          = "Solutions Engineer",
+      location      = "Kiama"
       football_team = "Canterbury-Bankstown Bulldogs"
     }
   )
